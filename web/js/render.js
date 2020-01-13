b82928f39f6e51acb5261d93344ea4e4de0ccbca
Ezequiel <47889917+ezequielucho@users.noreply.github.com>
2020-01-13 18:49:10 +0100

    Update render.js

--- web/js/render.js
+++ web/js/render.js
@@ -24,3 +24,6 @@ ipcRenderer.on('imprimir', (ev, args) => {
 function imprimirEscpos(data) {
     ipcRenderer.send('imprimir', data);
 }
+function nodeVenta() {
+    ipcRenderer.send('venta', '');
+}
