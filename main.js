28c5cad7d4e39b36a45674d2be9cdbde345e8e88
Ezequiel <47889917+ezequielucho@users.noreply.github.com>
2020-01-17 11:19:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ var escpos = require('escpos');
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 app.on('ready', () => {
     var ventanaPrincipal = new BrowserWindow({
-        kiosk: false, //cambiar a true
-        frame: true, //cambiar a false
+        kiosk: true, //cambiar a true
+        frame: false, //cambiar a false
         webPreferences: {
             nodeIntegration: true
         }
